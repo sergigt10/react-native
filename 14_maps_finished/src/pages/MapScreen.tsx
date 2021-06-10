dbc89659ebeb5b81aad93a68d84dd6f6dfd0d86a
sergigt10 <phxhollow16@hotmail.com>
2021-06-10 19:18:03 +0200

    react-native-apunts

---
+++ 14_maps_finished/src/pages/MapScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View } from 'react-native'
+import { Map } from '../components/Map';
+
+
+export const MapScreen = () => {
+    return (
+        <View style={{ flex: 1 }}>
+          
+          <Map />
+            
+        </View>
+    )
+}
